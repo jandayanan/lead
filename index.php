bf47dba44214a534170bec4092d2b80cbce16bb7
Jan Koichi Dayanan <jandayanan@gmail.com>
2013-04-12 00:21:05 +0800

    initial

---
+++ index.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="style.css">
+	<script src="jquery.js"></script>
+	<script src="bootstrap.js"></script>
+</head>
+<body>
+	<img src="banner.jpg" alt="" class="banner">
+	<div class='navbar navbar-inverse navbar-fixed'>
+		<div class='navbar-inner'>
+			<a href='#' class='brand'>Leaderboards</a>
+			<ul class='nav'>
+				<li><a href='#'>Sample</a></li>
+				<li><a href='#'>Links</a></li>
+				<li><a href='#'>Here</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class='wrap'>
+		<div class='row-fluid'>
+			<div class="span3">	
+				<ul class="nav nav-list"  data-spy="affix" data-offset-top="50">
+					<li><a href="#" class="btn"><i class='icon-chevron-right'></i>Module 1</a></li>
+					<li><a href="#" class="btn"><i class='icon-chevron-right'></i>Module 1</a></li>
+					<li><a href="#" class="btn"><i class='icon-chevron-right'></i>Module 1</a></li>
+					<li><a href="#" class="btn"><i class='icon-chevron-right'></i>Module 1</a></li>
+				</ul>
+			</div>
+			<div class="span9">
+				<div class="well">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, facere, asperiores, consequatur, nesciunt alias molestiae voluptatum odit reprehenderit illum numquam soluta quisquam expedita quaerat ab ullam voluptas aliquam illo cupiditate! <br>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, facere, asperiores, consequatur, nesciunt alias molestiae voluptatum odit reprehenderit illum numquam soluta quisquam expedita quaerat ab ullam voluptas aliquam illo cupiditate! <br>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, facere, asperiores, consequatur, nesciunt alias molestiae voluptatum odit reprehenderit illum numquam soluta quisquam expedita quaerat ab ullam voluptas aliquam illo cupiditate! <br>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, facere, asperiores, consequatur, nesciunt alias molestiae voluptatum odit reprehenderit illum numquam soluta quisquam expedita quaerat ab ullam voluptas aliquam illo cupiditate! <br>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, facere, asperiores, consequatur, nesciunt alias molestiae voluptatum odit reprehenderit illum numquam soluta quisquam expedita quaerat ab ullam voluptas aliquam illo cupiditate! <br>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, facere, asperiores, consequatur, nesciunt alias molestiae voluptatum odit reprehenderit illum numquam soluta quisquam expedita quaerat ab ullam voluptas aliquam illo cupiditate! <br>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, facere, asperiores, consequatur, nesciunt alias molestiae voluptatum odit reprehenderit illum numquam soluta quisquam expedita quaerat ab ullam voluptas aliquam illo cupiditate! <br>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, facere, asperiores, consequatur, nesciunt alias molestiae voluptatum odit reprehenderit illum numquam soluta quisquam expedita quaerat ab ullam voluptas aliquam illo cupiditate! <br>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, facere, asperiores, consequatur, nesciunt alias molestiae voluptatum odit reprehenderit illum numquam soluta quisquam expedita quaerat ab ullam voluptas aliquam illo cupiditate! <br>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, facere, asperiores, consequatur, nesciunt alias molestiae voluptatum odit reprehenderit illum numquam soluta quisquam expedita quaerat ab ullam voluptas aliquam illo cupiditate! <br>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, facere, asperiores, consequatur, nesciunt alias molestiae voluptatum odit reprehenderit illum numquam soluta quisquam expedita quaerat ab ullam voluptas aliquam illo cupiditate! <br>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, facere, asperiores, consequatur, nesciunt alias molestiae voluptatum odit reprehenderit illum numquam soluta quisquam expedita quaerat ab ullam voluptas aliquam illo cupiditate! <br>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, facere, asperiores, consequatur, nesciunt alias molestiae voluptatum odit reprehenderit illum numquam soluta quisquam expedita quaerat ab ullam voluptas aliquam illo cupiditate! <br>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, facere, asperiores, consequatur, nesciunt alias molestiae voluptatum odit reprehenderit illum numquam soluta quisquam expedita quaerat ab ullam voluptas aliquam illo cupiditate! <br>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, facere, asperiores, consequatur, nesciunt alias molestiae voluptatum odit reprehenderit illum numquam soluta quisquam expedita quaerat ab ullam voluptas aliquam illo cupiditate! <br>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, facere, asperiores, consequatur, nesciunt alias molestiae voluptatum odit reprehenderit illum numquam soluta quisquam expedita quaerat ab ullam voluptas aliquam illo cupiditate! <br>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, facere, asperiores, consequatur, nesciunt alias molestiae voluptatum odit reprehenderit illum numquam soluta quisquam expedita quaerat ab ullam voluptas aliquam illo cupiditate! <br>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, facere, asperiores, consequatur, nesciunt alias molestiae voluptatum odit reprehenderit illum numquam soluta quisquam expedita quaerat ab ullam voluptas aliquam illo cupiditate! <br>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="footer">
+		<p></p>
+	</div>
+	<div class='floating'>
+		<div class="floating well left">		
+		</div>
+		<div class="floating well right">asd</div>
+	</div>
+	<script>
+		$(document)
+			.ready(
+				function(){
+				}
+			);
+	</script>
+</body>
+</html>
